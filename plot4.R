5389448722479b0f7d99e0eeb0535bf6bf4ff054
guodu1995 <guodu.1995@gmail.com>
2017-10-31 14:31:27 -0400

    My answer

---
+++ plot4.R
@@ -0,0 +1,19 @@
+files <- file("household_power_consumption.txt")
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), 
+                   sep = ';', col.names = c("Date", "Time", "Global_active_power",
+                                            "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", 
+                                            "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
+par(mfrow = c(2,2))
+plot(data$DateTime, data$Global_active_power, xlab = "",ylab = 
+             "Global Active Power", type = "l")
+plot(data$DateTime, data$Voltage, xlab = "datetime", 
+     ylab = "Voltage", type = "l")
+plot(data$DateTime, data$Sub_metering_1, type = "l", ylab = "Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2,col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), legend = c(
+        "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
+plot(data$DateTime, data$Global_reactive_power, type = "l", 
+     xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
